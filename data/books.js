0b2894bde52e36fbbdf021a1fd6fa1b04912923d
zarumen <prinzbiz@gmail.com>
2020-03-18 17:39:17 +0700

    added book.js

---
+++ data/books.js
@@ -0,0 +1,284 @@
+export default [
+  {
+    "id": 1,
+    "book_name": "พ่อสอนลูก",
+    "book_link_pdf": "http://book.rgtcenter.com/004-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/004-01.txt"
+  },
+  {
+    "id": 2,
+    "book_name": "พรรษาวิสุทธิ์",
+    "book_link_pdf": "http://book.rgtcenter.com/013-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/013-01.txt"
+  },
+  {
+    "id": 3,
+    "book_name": "ของขวัญ",
+    "book_link_pdf": "http://book.rgtcenter.com/021-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/021-01.txt"
+  },
+  {
+    "id": 4,
+    "book_name": "เมื่อไม่รู้จะอ่านอะไร 1",
+    "book_link_pdf": "http://book.rgtcenter.com/028-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/028-01.txt"
+  },
+  {
+    "id": 5,
+    "book_name": "เมื่อไม่รู้จะอ่านอะไร 2",
+    "book_link_pdf": "http://book.rgtcenter.com/045-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/045-01.txt"
+  },
+  {
+    "id": 6,
+    "book_name": "เมื่อไม่รู้จะอ่านอะไร 3",
+    "book_link_pdf": "http://book.rgtcenter.com/063-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/063-01.txt"
+  },
+  {
+    "id": 7,
+    "book_name": "ชีวิตสมณะ..ที่สุดของชีวิตในสังสารวัฏ",
+    "book_link_pdf": "http://book.rgtcenter.com/030-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/030-01.txt"
+  },
+  {
+    "id": 8,
+    "book_name": "เลิศล้ำ ถ้อยคำครู",
+    "book_link_pdf": "http://book.rgtcenter.com/038-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/038-01.txt"
+  },
+  {
+    "id": 9,
+    "book_name": "เหล่ากอสมณะ",
+    "book_link_pdf": "http://book.rgtcenter.com/039-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/039-01.txt"
+  },
+  {
+    "id": 10,
+    "book_name": "ชีวิตสมณะ(ฉบับมหาปวารณา)",
+    "book_link_pdf": "http://book.rgtcenter.com/049-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/049-01.txt"
+  },
+  {
+    "id": 11,
+    "book_name": "ที่นี่มีคำตอบ 1",
+    "book_link_pdf": "http://book.rgtcenter.com/041-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 12,
+    "book_name": "ที่นี่มีคำตอบ 2",
+    "book_link_pdf": "http://book.rgtcenter.com/042-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 13,
+    "book_name": "ที่นี่มีคำตอบ 3",
+    "book_link_pdf": "http://book.rgtcenter.com/043-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 14,
+    "book_name": "ที่นี่มีคำตอบ 4",
+    "book_link_pdf": "http://book.rgtcenter.com/044-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 15,
+    "book_name": "ที่นี่มีคำตอบ 5",
+    "book_link_pdf": "http://book.rgtcenter.com/047-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 16,
+    "book_name": "ที่นี่มีคำตอบ 6",
+    "book_link_pdf": "http://book.rgtcenter.com/048-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 17,
+    "book_name": "ที่นี่มีคำตอบ 7",
+    "book_link_pdf": "http://book.rgtcenter.com/066-01.pdf",
+    "book_link_text": ""
+  },
+  {
+    "id": 18,
+    "book_name": "ที่นี่มีคำตอบ mini 1 เรื่องแปลกแต่จริง",
+    "book_link_pdf": "http://book.rgtcenter.com/050-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/050-01.txt"
+  },
+  {
+    "id": 19,
+    "book_name": "ที่นี่มีคำตอบ mini 2 ผู้หญิงต้องซื้อ",
+    "book_link_pdf": "http://book.rgtcenter.com/051-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/051-01.txt"
+  },
+  {
+    "id": 20,
+    "book_name": "ที่นี่มีคำตอบ mini 3 บุญคือเพื่อนซี้",
+    "book_link_pdf": "http://book.rgtcenter.com/052-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/052-01.txt"
+  },
+  {
+    "id": 21,
+    "book_name": "ที่นี่มีคำตอบ mini 4 รักนี้สีอะไร",
+    "book_link_pdf": "http://book.rgtcenter.com/053-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/053-01.txt"
+  },
+  {
+    "id": 22,
+    "book_name": "ที่นี่มีคำตอบ mini 5 อย่างงี้ บาปมั้ย",
+    "book_link_pdf": "http://book.rgtcenter.com/054-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/054-01.txt"
+  },
+  {
+    "id": 23,
+    "book_name": "ที่นี่มีคำตอบ mini 6 เป็นฉะนี้ เพราะไฉน",
+    "book_link_pdf": "http://book.rgtcenter.com/055-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/055-01.txt"
+  },
+  {
+    "id": 24,
+    "book_name": "ที่นี่มีคำตอบ mini 7 ชีวิตออกแบบได้",
+    "book_link_pdf": "http://book.rgtcenter.com/056-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/056-01.txt"
+  },
+  {
+    "id": 25,
+    "book_name": "ที่นี่มีคำตอบ mini 8 ความใช่ ความเชื่อ",
+    "book_link_pdf": "http://book.rgtcenter.com/057-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/057-01.txt"
+  },
+  {
+    "id": 26,
+    "book_name": "ที่นี่มีคำตอบ mini 9 เมื่อกรรมให้เกิด",
+    "book_link_pdf": "http://book.rgtcenter.com/058-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/058-01.txt"
+  },
+  {
+    "id": 27,
+    "book_name": "ที่นี่มีคำตอบ mini 10 บวชเถิดสร้างบุญญา",
+    "book_link_pdf": "http://book.rgtcenter.com/059-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/059-01.txt"
+  },
+  {
+    "id": 28,
+    "book_name": "ที่นี่มีคำตอบ mini 11 รู้ไว้..ใช่ว่า",
+    "book_link_pdf": "http://book.rgtcenter.com/060-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/060-01.txt"
+  },
+  {
+    "id": 29,
+    "book_name": "ที่นี่มีคำตอบ mini 12 เคลียร์ปัญหาครอบครัว",
+    "book_link_pdf": "http://book.rgtcenter.com/061-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/061-01.txt"
+  },
+  {
+    "id": 30,
+    "book_name": "ง่ายแต่ลึก 1",
+    "book_link_pdf": "http://book.rgtcenter.com/062-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/062-01.txt"
+  },
+  {
+    "id": 31,
+    "book_name": "ง่ายแต่ลึก 2",
+    "book_link_pdf": "http://book.rgtcenter.com/065-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/065-01.txt"
+  },
+  {
+    "id": 32,
+    "book_name": "ง่ายแต่ลึก 3",
+    "book_link_pdf": "http://book.rgtcenter.com/071-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/071-01.txt"
+  },
+  {
+    "id": 33,
+    "book_name": "บางสิ่งที่แสวงหา",
+    "book_link_pdf": "http://book.rgtcenter.com/064-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/064-01.txt"
+  },
+  {
+    "id": 34,
+    "book_name": "ผ้าสีสุดท้าย",
+    "book_link_pdf": "http://book.rgtcenter.com/070-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/070-01.txt"
+  },
+  {
+    "id": 35,
+    "book_name": "คำไม่เล็กของคุณครูไม่ใหญ่ 1",
+    "book_link_pdf": "http://book.rgtcenter.com/067-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/067-01.txt"
+  },
+  {
+    "id": 36,
+    "book_name": "คำไม่เล็กฯ 1 (บุญเท่านั้นเป็นที่พึ่ง)",
+    "book_link_pdf": "http://book.rgtcenter.com/072-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/072-01.txt"
+  },
+  {
+    "id": 37,
+    "book_name": "คำไม่เล็กฯ 2 (เกิดมาสร้างบารมี)",
+    "book_link_pdf": "http://book.rgtcenter.com/073-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/073-01.txt"
+  },
+  {
+    "id": 38,
+    "book_name": "คำไม่เล็กฯ 3 (ศีลธรรมรุ่งเรือง...สังคมร่มเย็น)",
+    "book_link_pdf": "http://book.rgtcenter.com/074-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/074-01.txt"
+  },
+  {
+    "id": 39,
+    "book_name": "คำไม่เล็กฯ 4 (สมาธิ...ลมหายใจของชีวิต)",
+    "book_link_pdf": "http://book.rgtcenter.com/075-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/075-01.txt"
+  },
+  {
+    "id": 40,
+    "book_name": "คำไม่เล็กฯ 5 (ศิลปะแห่งสมาธิ)",
+    "book_link_pdf": "http://book.rgtcenter.com/076-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/076-01.txt"
+  },
+  {
+    "id": 41,
+    "book_name": "คำสอนคุณครูไม่ใหญ่ 1",
+    "book_link_pdf": "http://book.rgtcenter.com/078-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/078-01.txt"
+  },
+  {
+    "id": 42,
+    "book_name": "คำสอนคุณครูไม่ใหญ่ 2",
+    "book_link_pdf": "http://book.rgtcenter.com/082-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/082-01.txt"
+  },
+  {
+    "id": 43,
+    "book_name": "คำสอนคุณครูไม่ใหญ่ 3",
+    "book_link_pdf": "http://book.rgtcenter.com/083-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/083-01.txt"
+  },
+  {
+    "id": 44,
+    "book_name": "ทำพระนิพพานให้แจ้ง (คำมินิ)",
+    "book_link_pdf": "http://book.rgtcenter.com/079-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/079-01.txt"
+  },
+  {
+    "id": 45,
+    "book_name": "แสวงบุญ (คำมินิ)",
+    "book_link_pdf": "http://book.rgtcenter.com/080-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/080-01.txt"
+  },
+  {
+    "id": 46,
+    "book_name": "สร้างบารมี (คำมินิ)",
+    "book_link_pdf": "http://book.rgtcenter.com/081-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/081-01.txt"
+  },
+  {
+    "id": 47,
+    "book_name": "คำตอบคุณครูไม่ใหญ่ เล่ม 1",
+    "book_link_pdf": "http://book.rgtcenter.com/084-01.pdf",
+    "book_link_text": "http://book.rgtcenter.com/084-01.txt"
+  }
+]
