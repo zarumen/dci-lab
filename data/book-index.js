fb1dcdba1685905b94f5838558abe9295fc3c7df
zarumen <prinzbiz@gmail.com>
2020-03-18 13:37:57 +0700

    added book.rgtcenter.com data

---
+++ data/book-index.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    "id": 1,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "หัวใจพระบรมโพธิสัตว์",
+    "link_pdf": "http://book.rgtcenter.com/083-01-01.pdf"
+  },
+  {
+    "id": 2,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "ชนะด้วยการคิดดี พูดดี ทำดี",
+    "link_pdf": "http://book.rgtcenter.com/083-01-02.pdf"
+  },
+  {
+    "id": 3,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "วันนี้คุณทำความดีแล้วหรือยัง",
+    "link_pdf": "http://book.rgtcenter.com/083-01-03.pdf"
+  },
+  {
+    "id": 4,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "มาปลูกเมตตาธรรมในใจกันเถอะ",
+    "link_pdf": "http://book.rgtcenter.com/083-01-04.pdf"
+  },
+  {
+    "id": 5,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "สั่งสมธาตุบริสุทธิ์ในตัวเรา",
+    "link_pdf": "http://book.rgtcenter.com/083-01-05.pdf"
+  },
+  {
+    "id": 6,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "อดีตที่ผิดพลาด...ลืมให้หมด",
+    "link_pdf": "http://book.rgtcenter.com/083-01-06.pdf"
+  },
+  {
+    "id": 7,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "อารมณ์ใส อย่าปล่อยให้เสีย",
+    "link_pdf": "http://book.rgtcenter.com/083-01-07.pdf"
+  },
+  {
+    "id": 8,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "การแก้แค้นที่ถูกวิธี",
+    "link_pdf": "http://book.rgtcenter.com/083-01-08.pdf"
+  },
+  {
+    "id": 9,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "ทำตัวประดุจผ้าขี้ริ้ว",
+    "link_pdf": "http://book.rgtcenter.com/083-01-09.pdf"
+  },
+  {
+    "id": 10,
+    "book_name": "คำสอนคุณครูไม่ใหญ่3",
+    "index_name": "อันตัวเรานั้น...ตายแน่ ๆ",
+    "link_pdf": "http://book.rgtcenter.com/083-01-10.pdf"
+  }
+]
